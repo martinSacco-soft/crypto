8e87f04555769ee03af3e7ab1a2022f90745a068
Martin Sacco <martin.sacco@softvision.com>
2020-02-07 17:57:32 -0300

    Added folders and service skeleton

---
+++ src/main/java/org/crypto/web/CryptoController.java
@@ -0,0 +1,32 @@
+package org.crypto.web;
+
+public class CryptoController {
+
+	public void getAllCryptoCurrencies () {
+	
+	}
+	
+	public void createWallet () {
+	
+	}
+	
+	public void getWallet () {
+	
+	}
+	
+	public void updateWallet() {
+	
+	}
+	
+	public void removeWallet() {
+	
+	}
+	
+	public void buyCurrency() {
+	
+	}
+	
+	public void transferValues() {
+	
+	}
+}
